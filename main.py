980d244d5eb6f4b4e5da3b6f5ec31c486e1a28bb
iiiiii1wepfj <18629298+iiiiii1wepfj@users.noreply.github.com>
2021-03-03 16:09:51 +0200

    add db

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
 from pyrogram import Client, filters
 from gpytranslate import Translator
-import string
-
-lang = 'en'
+import sqlite3, string 
 
 bot = Client(
     "APP_NAME",
@@ -11,6 +9,53 @@ bot = Client(
     bot_token= "TOKEN"
 )
 
+db = sqlite3.connect("userlanguages.db")
+dbc = db.cursor()
+dbc.execute("""CREATE TABLE IF NOT EXISTS users (user_id INTEGER PRIMARY KEY,
+                                                 chat_lang)""")
+db.commit()
+
+default_language = "en"
+
+def chat_exists(chat_id, chat_type):
+    if chat_type == "private":
+        dbc.execute("SELECT user_id FROM users where user_id = ?", (chat_id,))
+        return bool(dbc.fetchone())
+    raise TypeError("Unknown chat type '%s'." % chat_type)
+
+    
+def get_db_lang(chat_id: int, chat_type: str) -> str:
+    if chat_type == "private":
+        dbc.execute("SELECT chat_lang FROM users WHERE user_id = ?", (chat_id,))
+        ul = dbc.fetchone()
+    return ul[0] if ul else None
+    
+def add_chat(chat_id, chat_type):
+    if chat_type == "private":
+        dbc.execute("INSERT INTO users (user_id) values (?)", (chat_id,))
+        db.commit()
+        
+        
+def set_db_lang(chat_id: int, chat_type: str, lang_code: str):
+    if chat_type == "private":
+        dbc.execute("UPDATE users SET chat_lang = ? WHERE user_id = ?", (lang_code, chat_id))
+        db.commit()
+
+
+        
+   
+@bot.on_message(filters.private, group=-1)
+async def check_chat(bot, msg):
+    chat_id = msg.chat.id
+    chat_type = msg.chat.type
+
+    if not chat_exists(chat_id, chat_type):
+        add_chat(chat_id, chat_type)
+        set_db_lang(chat_id, chat_type, "en")
+        
+        
+
+    
 ##Configure welcome message
 @bot.on_message(filters.command("start") & filters.private)
 async def start(bot, msg):
@@ -32,16 +77,12 @@ async def language(bot, msg):
     await msg.reply_text(f"**Languages**\n\n__Select the language you want to translate.__\n\n•/lang (language code) \n\nExample: ```/lang en``` \n\nList of language codes: https://cloud.google.com/translate/docs/languages   \n\n Send the relevant command. \ud83e\udd20")
 
 
+
 @bot.on_message(filters.command("lang") & filters.private)
-async def setlanguage(bot, msg):
-    global lang
-    lang = msg.command[1]
-    @bot.on_message(filters.private & filters.private)
-    async def main(bot, msg):
-        tr = Translator()
-        translation = await tr(msg.text, targetlang=[lang, 'utf-16'])
-        language = await tr.detect(msg.text)
-        await msg.reply(f"**\ud83c\udf10 Translation**:\n\n```{translation.text}```\n\n**🔍 Detected language:** {language}")
+async def setmylang(bot, msg):
+ thelang = msg.command[1]
+ set_db_lang(msg.chat.id, msg.chat.type, thelang)
+
 
 
 
@@ -49,11 +90,9 @@ async def setlanguage(bot, msg):
 @bot.on_message(filters.private)
 async def main(bot, msg):
     tr = Translator()
-    translation = await tr(msg.text, targetlang=[lang, 'utf-16'])
+    userlang = get_db_lang(msg.chat.id, msg.chat.type)
+    translation = await tr(msg.text, targetlang=[userlang, 'utf-16'])
     language = await tr.detect(msg.text)
     await msg.reply(f"**\ud83c\udf10 Translation**:\n\n```{translation.text}```\n\n**🔍 Detected language:** {language}")
-
-
-
 
 bot.run()
