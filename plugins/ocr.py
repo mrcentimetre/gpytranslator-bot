2f847da89666d6fb9f8f4800a231fc6fdd2b848e
iiiiii1wepfj <18629298+iiiiii1wepfj@users.noreply.github.com>
2021-03-17 09:18:09 +0200

    Update ocr.py

--- plugins/ocr.py
+++ plugins/ocr.py
@@ -1,14 +1,15 @@
 from pyrogram import Client, filters
 from pyrogram.types import Message
 import pytesseract, PIL, os
+import constants
 
 @Client.on_message(filters.command("ocr"))
 async def ocrcmd(bot, message: Message):
  if not message.reply_to_message:
-  await message.reply("reply to a photo to get the text")
+  await message.reply(constants.error_ocr_no_reply)
   return
  if not message.reply_to_message.photo:
-  await message.reply("reply to a photo to get the text")
+  await message.reply(constants.error_ocr_no_reply)
   return
  if message.reply_to_message.photo:
     await message.reply_to_message.download(file_name='ocr.jpg')
