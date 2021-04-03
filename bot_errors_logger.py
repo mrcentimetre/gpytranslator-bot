49f729c4e1e003665af00dc1301a5af6cef5b89b
iiiiii1wepfj <18629298+iiiiii1wepfj@users.noreply.github.com>
2021-04-03 19:44:12 +0300

    Update bot_errors_logger.py

--- bot_errors_logger.py
+++ bot_errors_logger.py
@@ -11,12 +11,3 @@ def logging_errors(f):
         except Exception as e:
             await message.reply(f"**Error:**  \n\n ```{e}``` \n\n **forward this message to https://t.me/TDICSupport if you see this error again**", parse_mode="markdown")
     return err_log
-
-def logging_errors_inline(f):
-    @wraps(f)
-    async def err_log(client: Client, query: InlineQuery, *args, **kwargs):
-        try:
-            return await f(client, query, *args, **kwargs)
-        except Exception as e:
-            await query.answer(f"**Error:**  \n\n ```{e}``` \n\n **forward this message to https://t.me/TDICSupport if you see this error again**", parse_mode="markdown")
-    return err_log
