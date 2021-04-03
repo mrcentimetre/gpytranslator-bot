9817034f396011fb42029bb7fc03faa8e2d13d0d
iiiiii1wepfj <18629298+iiiiii1wepfj@users.noreply.github.com>
2021-04-03 20:18:16 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
 from pyrogram import Client, filters
 from config import API_ID, API_HASH, TOKEN, sudofilter
-
+import os, sys
+from threading import Thread
+from datetime import datetime
 
 bot = Client(
     ":memory:",
@@ -9,6 +11,30 @@ bot = Client(
     bot_token=TOKEN,
     plugins=dict(root="plugins")
 )
+
+def stop_and_restart():
+    bot.stop()
+    os.system("git pull")
+    os.execl(sys.executable, sys.executable, *sys.argv)
+
+
+@bot.on_message(filters.command("r") & sudofilter & ~filters.forwarded & ~filters.group & ~filters.edited & ~filters.via_bot)
+async def restart(bot, message):
+    Thread(
+        target=stop_and_restart
+    ).start()
+
+
+@bot.on_message(filters.command("getbotdb") & sudofilter & ~filters.forwarded & ~filters.group & ~filters.edited & ~filters.via_bot)
+async def send_the_db(bot, message):
+ await message.reply_document("userlanguages.db", thumb="botprofilepic.jpg")
+
+@bot.on_message(filters.command("ping") & sudofilter)
+async def ping(bot, message):
+ a = datetime.now()
+ m = await message.reply_text("pong")
+ b = datetime.now()
+ await m.edit_text(f"pong {(b - a).microseconds / 1000} ms")
 
 
 bot.run()
