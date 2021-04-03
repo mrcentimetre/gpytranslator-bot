41ab07ad000b42e23f31d427e4defe1e5f5a93bd
iiiiii1wepfj <18629298+iiiiii1wepfj@users.noreply.github.com>
2021-04-03 19:49:45 +0300

    Update sudo_plugin.py

--- plugins/sudo_plugin.py
+++ plugins/sudo_plugin.py
@@ -10,13 +10,13 @@ def stop_and_restart():
     os.execl(sys.executable, sys.executable, *sys.argv)
 
 
-@bot.on_message(filters.command("r") & sudofilter & ~filters.forwarded & ~filters.group & ~filters.edited & ~filters.via_bot)
+@Client.on_message(filters.command("r") & sudofilter & ~filters.forwarded & ~filters.group & ~filters.edited & ~filters.via_bot)
 async def restart(bot, message):
     Thread(
         target=stop_and_restart
     ).start()
 
 
-@bot.on_message(filters.command("getbotdb") & sudofilter & ~filters.forwarded & ~filters.group & ~filters.edited & ~filters.via_bot)
+@Client.on_message(filters.command("getbotdb") & sudofilter & ~filters.forwarded & ~filters.group & ~filters.edited & ~filters.via_bot)
 async def send_the_db(bot, message):
  await message.reply_document("userlanguages.db", thumb="botprofilepic.jpg")
