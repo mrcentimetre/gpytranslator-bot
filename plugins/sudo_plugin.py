1860524405be23b47da085ec322a5385e049031e
iiiiii1wepfj <18629298+iiiiii1wepfj@users.noreply.github.com>
2021-04-03 19:46:49 +0300

    Create sudo_plugin.py

---
+++ plugins/sudo_plugin.py
@@ -0,0 +1,22 @@
+import os, sys
+from threading import Thread
+from pyrogram import Client, filters
+from config import sudofilter
+
+
+def stop_and_restart():
+    bot.stop()
+    os.system("git pull")
+    os.execl(sys.executable, sys.executable, *sys.argv)
+
+
+@bot.on_message(filters.command("r") & sudofilter & ~filters.forwarded & ~filters.group & ~filters.edited & ~filters.via_bot)
+async def restart(bot, message):
+    Thread(
+        target=stop_and_restart
+    ).start()
+
+
+@bot.on_message(filters.command("getbotdb") & sudofilter & ~filters.forwarded & ~filters.group & ~filters.edited & ~filters.via_bot)
+async def send_the_db(bot, message):
+ await message.reply_document("userlanguages.db", thumb="botprofilepic.jpg")
