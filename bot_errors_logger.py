6c93721c9425cfa732514f717044ca0dba4f6670
iiiiii1wepfj <18629298+iiiiii1wepfj@users.noreply.github.com>
2021-04-10 09:29:00 +0300

    Update bot_errors_logger.py

--- bot_errors_logger.py
+++ bot_errors_logger.py
@@ -1,7 +1,7 @@
 from pyrogram import Client
 from pyrogram.types import Message, InlineQuery
 from functools import wraps
-
+import constants
 
 def logging_errors(f):
     @wraps(f)
@@ -9,5 +9,5 @@ def logging_errors(f):
         try:
             return await f(client, message, *args, **kwargs)
         except Exception as e:
-            await message.reply(f"**Error:**  \n\n ```{e}``` \n\n **forward this message to https://t.me/TDICSupport if you see this error again**", parse_mode="markdown")
+            await message.reply(f"**Error:**  \n\n ```{e}``` \n\n **forward this message to https://t.me/TDICSupport if you see this error again**", parse_mode="markdown", reply_markup=constants.error_message_markup)
     return err_log
