807f7d6b0b113aa435e18a80fea040678b60cbd3
iiiiii1wepfj <18629298+iiiiii1wepfj@users.noreply.github.com>
2021-04-10 09:39:40 +0300

    Update bot_errors_logger.py

--- bot_errors_logger.py
+++ bot_errors_logger.py
@@ -1,5 +1,6 @@
 from pyrogram import Client
 from pyrogram.types import Message, InlineQuery
+from pyrogram.errors.exceptions.forbidden_403 import ChatWriteForbidden
 from functools import wraps
 import constants
 
@@ -8,6 +9,9 @@ def logging_errors(f):
     async def err_log(client: Client, message: Message, *args, **kwargs):
         try:
             return await f(client, message, *args, **kwargs)
+        except ChatWriteForbidden:
+            await client.leave_chat(message.chat.id)
+            return
         except Exception as e:
             await message.reply(f"**Error:**  \n\n ```{e}``` \n\n **forward this message to https://t.me/TDICSupport if you see this error again**", parse_mode="markdown", reply_markup=constants.error_message_markup)
     return err_log
