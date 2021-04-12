0df02186e0559ea9c8b7c59b3554c6a62dc7fd50
iiiiii1wepfj <18629298+iiiiii1wepfj@users.noreply.github.com>
2021-04-12 08:17:24 +0300

    Update inline.py

--- plugins/inline.py
+++ plugins/inline.py
@@ -1,5 +1,9 @@
 from pyrogram import Client
-from pyrogram.types import InlineQuery, InlineQueryResultArticle, InputTextMessageContent
+from pyrogram.types import (
+    InlineQuery,
+    InlineQueryResultArticle,
+    InputTextMessageContent,
+)
 import constants
 from tr import tr
 
@@ -10,18 +14,14 @@ async def translateinline(bot: Client, query: InlineQuery) -> None:
         to_translate = query.query.lower().split(None, 1)[1]
         language = await tr.detect(query.query.lower().split(None, 1)[1])
         tolanguage = query.query.lower().split()[0]
-        translation = await tr(
-            to_translate,
-            sourcelang=language,
-            targetlang=tolanguage
-        )
+        translation = await tr(to_translate, sourcelang=language, targetlang=tolanguage)
         trmsgtext = f"{translation.text}"
         await query.answer(
             [
                 InlineQueryResultArticle(
                     title=constants.inline_text_string_one.format(language, tolanguage),
                     description=f"{translation.text}",
-                    input_message_content=InputTextMessageContent(trmsgtext)
+                    input_message_content=InputTextMessageContent(trmsgtext),
                 )
             ]
         )
