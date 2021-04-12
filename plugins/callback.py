16984be496589f689b405f1416e411a5fdaaba3e
iiiiii1wepfj <18629298+iiiiii1wepfj@users.noreply.github.com>
2021-04-12 08:18:29 +0300

    Update callback.py

--- plugins/callback.py
+++ plugins/callback.py
@@ -7,16 +7,13 @@ import constants
 async def backtostart(bot: Client, query: CallbackQuery):
     await query.message.edit(
         constants.start_message_text.format(query.from_user.mention()),
-        reply_markup=constants.start_message_reply_markup
+        reply_markup=constants.start_message_reply_markup,
     )
 
 
 @Client.on_callback_query(filters.regex(r"^help$"))
 async def helpbutton(bot: Client, query: CallbackQuery):
-    await query.message.edit(
-        constants.help_text,
-        reply_markup=constants.help_markup
-    )
+    await query.message.edit(constants.help_text, reply_markup=constants.help_markup)
 
 
 @Client.on_callback_query(filters.regex(r"^Credits$"))
