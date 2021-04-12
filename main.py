f27c42c3255041075779f9b8e0a621b79d975679
iiiiii1wepfj <18629298+iiiiii1wepfj@users.noreply.github.com>
2021-04-12 08:14:30 +0300

    black

--- main.py
+++ main.py
@@ -9,8 +9,9 @@ bot = Client(
     api_id=API_ID,
     api_hash=API_HASH,
     bot_token=TOKEN,
-    plugins=dict(root="plugins")
+    plugins=dict(root="plugins"),
 )
+
 
 def stop_and_restart():
     bot.stop()
@@ -18,23 +19,36 @@ def stop_and_restart():
     os.execl(sys.executable, sys.executable, *sys.argv)
 
 
-@bot.on_message(filters.command("r") & sudofilter & ~filters.forwarded & ~filters.group & ~filters.edited & ~filters.via_bot)
+@bot.on_message(
+    filters.command("r")
+    & sudofilter
+    & ~filters.forwarded
+    & ~filters.group
+    & ~filters.edited
+    & ~filters.via_bot
+)
 async def restart(bot, message):
-    Thread(
-        target=stop_and_restart
-    ).start()
+    Thread(target=stop_and_restart).start()
 
 
-@bot.on_message(filters.command("getbotdb") & sudofilter & ~filters.forwarded & ~filters.group & ~filters.edited & ~filters.via_bot)
+@bot.on_message(
+    filters.command("getbotdb")
+    & sudofilter
+    & ~filters.forwarded
+    & ~filters.group
+    & ~filters.edited
+    & ~filters.via_bot
+)
 async def send_the_db(bot, message):
- await message.reply_document("userlanguages.db", thumb="botprofilepic.jpg")
+    await message.reply_document("userlanguages.db", thumb="botprofilepic.jpg")
+
 
 @bot.on_message(filters.command("ping") & sudofilter & filters.private)
 async def ping(bot, message):
- a = datetime.now()
- m = await message.reply_text("pong")
- b = datetime.now()
- await m.edit_text(f"pong {(b - a).microseconds / 1000} ms")
+    a = datetime.now()
+    m = await message.reply_text("pong")
+    b = datetime.now()
+    await m.edit_text(f"pong {(b - a).microseconds / 1000} ms")
 
 
 bot.run()
