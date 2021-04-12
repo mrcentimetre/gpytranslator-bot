f40a5b30f1970f787c65a22bc091a6f613b442bd
iiiiii1wepfj <18629298+iiiiii1wepfj@users.noreply.github.com>
2021-04-12 08:17:00 +0300

    Update ocr.py

--- plugins/ocr.py
+++ plugins/ocr.py
@@ -10,13 +10,17 @@ prefix = constants.prefix
 @Client.on_message(filters.command("ocr", prefix))
 @logging_errors
 async def ocrcmd(bot, message: Message):
- if not message.reply_to_message:
-  await message.reply(constants.error_ocr_no_reply)
-  return
- if not message.reply_to_message.photo:
-  await message.reply(constants.error_ocr_no_reply)
-  return
- if message.reply_to_message.photo:
-    await message.reply_to_message.download(file_name='ocr.jpg')
-    await message.reply(constants.ocr_message_text.format(pytesseract.image_to_string(PIL.Image.open('downloads/ocr.jpg'))))
-    os.remove("downloads/ocr.jpg")
+    if not message.reply_to_message:
+        await message.reply(constants.error_ocr_no_reply)
+        return
+    if not message.reply_to_message.photo:
+        await message.reply(constants.error_ocr_no_reply)
+        return
+    if message.reply_to_message.photo:
+        await message.reply_to_message.download(file_name="ocr.jpg")
+        await message.reply(
+            constants.ocr_message_text.format(
+                pytesseract.image_to_string(PIL.Image.open("downloads/ocr.jpg"))
+            )
+        )
+        os.remove("downloads/ocr.jpg")
