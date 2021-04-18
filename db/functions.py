2359a4290d10273c2934f2dc01e726efbf7463ce
iiiiii1wepfj <18629298+iiiiii1wepfj@users.noreply.github.com>
2021-04-18 14:37:08 +0300

    add a command to get the users count

--- db/functions.py
+++ db/functions.py
@@ -26,3 +26,9 @@ def set_lang(chat_id: int, chat_type: str, lang_code: str):
         dbc.execute(
             "UPDATE users SET chat_lang = ? WHERE user_id = ?", (lang_code, chat_id))
         db.commit()
+
+
+def get_users_count():
+    a = dbc.execute("""select count(*) from users""")
+    b = a.fetchone()[0]
+    return f"the bot have {b} users"
