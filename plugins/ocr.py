40ed44da3d71da873a738bd34c1c0684ff3962c9
iiiiii1wepfj <18629298+iiiiii1wepfj@users.noreply.github.com>
2021-06-09 19:12:08 +0300

    Update ocr.py

--- plugins/ocr.py
+++ plugins/ocr.py
@@ -7,6 +7,13 @@ from bot_errors_logger import logging_errors
 prefix = constants.prefix
 
 
+def getocrlangsasalist():
+    a = ""
+    for i in pytesseract.get_languages():
+        a += f"{i}\n"
+    return a
+
+
 @Client.on_message(filters.command("ocr", prefix))
 @logging_errors
 async def ocrcmd(bot, message: Message):
@@ -30,21 +37,23 @@ async def ocrcmd(bot, message: Message):
 @logging_errors
 async def ocrlangcmd(bot, message: Message):
     if len(message.text.split()) > 1:
+        msg_arg = message.command[1]
         if not message.reply_to_message:
-            await message.reply(constants.error_ocr_no_reply)
-            return
+            return await message.reply(constants.error_ocr_no_reply)
         if not message.reply_to_message.photo:
-            await message.reply(constants.error_ocr_no_reply)
-            return
+            return await message.reply(constants.error_ocr_no_reply)
         if message.reply_to_message.photo:
-            await message.reply_to_message.download(file_name="ocr.jpg")
-            await message.reply(
-                constants.ocr_message_text.format(
-                    pytesseract.image_to_string(
-                        PIL.Image.open("downloads/ocr.jpg"), lang=message.command[1]
+            if msg_arg in pytesseract.get_languages():
+                await message.reply_to_message.download(file_name="ocr.jpg")
+                await message.reply(
+                    constants.ocr_message_text.format(
+                        pytesseract.image_to_string(
+                            PIL.Image.open("downloads/ocr.jpg"), lang=msg_arg
+                        )
                     )
                 )
-            )
-            os.remove("downloads/ocr.jpg")
+                os.remove("downloads/ocr.jpg")
+            else:
+                await message.reply(constants.ocr_err_msg_lang)
     else:
         await ocrcmd(bot, message)
