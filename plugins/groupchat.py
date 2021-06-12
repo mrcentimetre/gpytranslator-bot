5b335f18283efddb4c05ad745ce7598b005ad00d
iiiiii1wepfj <18629298+iiiiii1wepfj@users.noreply.github.com>
2021-06-12 16:20:47 +0300

    Update groupchat.py

--- plugins/groupchat.py
+++ plugins/groupchat.py
@@ -3,7 +3,7 @@ from pyrogram.types import Message, InlineKeyboardMarkup, InlineKeyboardButton
 import constants
 from tr import tr
 from bot_errors_logger import logging_errors
-
+from bot_custom_exceptions import bot_custom_exceptions
 
 prefix = constants.prefix
 
@@ -13,57 +13,71 @@ prefix = constants.prefix
 )
 @logging_errors
 async def translategroup(bot, message: Message) -> None:
-    if message.reply_to_message.poll is None:
-        if message.reply_to_message.caption:
-            to_translate = message.reply_to_message.caption
-        elif message.reply_to_message.text:
-            to_translate = message.reply_to_message.text
-        try:
-            args = message.text.split()[1].lower()
-            if "//" in args:
-                language = args.split("//")[0]
-                tolanguage = args.split("//")[1]
-            else:
+    try:
+        if message.reply_to_message.poll is None:
+            if message.reply_to_message.caption:
+                to_translate = message.reply_to_message.caption
+            elif message.reply_to_message.text:
+                to_translate = message.reply_to_message.text
+            try:
+                args = message.text.split()[1].lower()
+                if "//" in args:
+                    language = args.split("//")[0]
+                    tolanguage = args.split("//")[1]
+                else:
+                    language = await tr.detect(to_translate)
+                    tolanguage = args
+            except IndexError:
                 language = await tr.detect(to_translate)
-                tolanguage = args
-        except IndexError:
+                tolanguage = "en"
+            translation = await tr(
+                to_translate, sourcelang=language, targetlang=tolanguage
+            )
+            await message.reply(
+                constants.translate_string_one.format(
+                    translation.text, language, tolanguage
+                ),
+                parse_mode="markdown",
+            )
+        elif message.reply_to_message.poll is not None:
+            options = "\n".join(
+                x["text"] for x in message.reply_to_message.poll.options
+            )
+            to_translate = f"{message.reply_to_message.poll.question}\n\n\n{options}"
             language = await tr.detect(to_translate)
-            tolanguage = "en"
-        translation = await tr(to_translate, sourcelang=language, targetlang=tolanguage)
-        await message.reply(
-            constants.translate_string_one.format(
-                translation.text, language, tolanguage
-            ),
-            parse_mode="markdown",
-        )
-    elif message.reply_to_message.poll is not None:
-        options = "\n".join(x["text"] for x in message.reply_to_message.poll.options)
-        to_translate = f"{message.reply_to_message.poll.question}\n\n\n{options}"
-        language = await tr.detect(to_translate)
-        if len(message.text.split()) > 1:
-            tolanguage = message.command[1]
-        else:
-            tolanguage = "en"
-        translation = await tr(to_translate, sourcelang=language, targetlang=tolanguage)
-        await message.reply(
-            constants.translate_string_one.format(
-                translation.text, language, tolanguage
-            ),
-            parse_mode="markdown",
-        )
+            if len(message.text.split()) > 1:
+                tolanguage = message.command[1]
+            else:
+                tolanguage = "en"
+            translation = await tr(
+                to_translate, sourcelang=language, targetlang=tolanguage
+            )
+            await message.reply(
+                constants.translate_string_one.format(
+                    translation.text, language, tolanguage
+                ),
+                parse_mode="markdown",
+            )
+    except json.decoder.JSONDecodeError:
+        raise bot_custom_exceptions(constants.google_tr_api_err_msg)
 
 
 @Client.on_message(filters.command("tr", prefix) & filters.group & ~filters.reply)
 @logging_errors
 async def translategrouptwo(bot, message: Message):
-    to_translate = message.text.split(None, 2)[2]
-    language = await tr.detect(message.text.split(None, 2)[2])
-    tolanguage = message.command[1]
-    translation = await tr(to_translate, sourcelang=language, targetlang=tolanguage)
-    await message.reply(
-        constants.translate_string_one.format(translation.text, language, tolanguage),
-        parse_mode="markdown",
-    )
+    try:
+        to_translate = message.text.split(None, 2)[2]
+        language = await tr.detect(message.text.split(None, 2)[2])
+        tolanguage = message.command[1]
+        translation = await tr(to_translate, sourcelang=language, targetlang=tolanguage)
+        await message.reply(
+            constants.translate_string_one.format(
+                translation.text, language, tolanguage
+            ),
+            parse_mode="markdown",
+        )
+    except json.decoder.JSONDecodeError:
+        raise bot_custom_exceptions(constants.google_tr_api_err_msg)
 
 
 @Client.on_message(
