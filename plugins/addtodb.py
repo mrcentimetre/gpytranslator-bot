52791ee945a29cae7cff5ab9bdf14f91c6126ca3
itay <anon@whatever>
2021-06-22 16:16:49 +0300

    black code format

--- plugins/addtodb.py
+++ plugins/addtodb.py
@@ -2,6 +2,7 @@ from pyrogram import Client, filters
 from pyrogram.types import Message
 from db import functions as db
 
+
 @Client.on_message(filters.private, group=-1)
 async def check_chat(bot: Client, message: Message):
     chat_id = message.chat.id
