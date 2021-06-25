31fd9db914886a5a38c74a8daaf65d77665849c4
iiiiii1wepfj <18629298+iiiiii1wepfj@users.noreply.github.com>
2021-06-25 17:11:15 +0300

    Update db.py

--- db/db.py
+++ db/db.py
@@ -1,14 +1,11 @@
-from tortoise import Tortoise, fields
-from tortoise.models import Model
+import sqlite3
 
 
-class users(Model):
-    user_id = fields.IntField(pk=True)
-    chat_lang = fields.TextField()
-
-
-async def init_db():
-    await Tortoise.init(
-        db_url="sqlite://userlanguages.sqlite", modules={"models": [__name__]}
-    )
-    await Tortoise.generate_schemas()
+db = sqlite3.connect("userlanguages.db")
+dbc = db.cursor()
+dbc.execute("""
+CREATE TABLE IF NOT EXISTS users (
+    user_id INTEGER PRIMARY KEY,
+    chat_lang
+)""")
+db.commit()
