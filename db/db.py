833934d6e5e37dfa3e59c290d9c5515232d01be7
iiiiii1wepfj <18629298+iiiiii1wepfj@users.noreply.github.com>
2021-06-26 12:46:58 +0300

    Update db.py

--- db/db.py
+++ db/db.py
@@ -5,7 +5,7 @@ db = sqlite3.connect("userlanguages.db")
 dbc = db.cursor()
 dbc.execute("""
 CREATE TABLE IF NOT EXISTS users (
-    user_id BIGINT PRIMARY KEY,
+    user_id INTEGER PRIMARY KEY,
     chat_lang
 )""")
 db.commit()
