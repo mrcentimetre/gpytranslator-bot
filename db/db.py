ce52e06b54267e6e186b125ac0e0f40915bc3810
iiiiii1wepfj <18629298+iiiiii1wepfj@users.noreply.github.com>
2021-07-04 11:07:06 +0300

    Update db.py

--- db/db.py
+++ db/db.py
@@ -1,6 +1,6 @@
 from tortoise import Tortoise, fields
 from tortoise.models import Model
-
+from config import db_url
 
 class users(Model):
     user_id = fields.IntField(pk=True)
@@ -9,6 +9,6 @@ class users(Model):
 
 async def init_db():
     await Tortoise.init(
-        db_url="sqlite://userlanguages.db", modules={"models": [__name__]}
+        db_url=db_url, modules={"models": [__name__]}
     )
     await Tortoise.generate_schemas()
