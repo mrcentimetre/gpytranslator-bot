f97ec3549e5a27e6efd7d91b95a23a3f27e11ed1
iiiiii1wepfj <18629298+iiiiii1wepfj@users.noreply.github.com>
2021-07-04 11:12:25 +0300

    Update db.py

--- db/db.py
+++ db/db.py
@@ -2,13 +2,12 @@ from tortoise import Tortoise, fields
 from tortoise.models import Model
 from config import db_url
 
+
 class users(Model):
     user_id = fields.IntField(pk=True)
     chat_lang = fields.TextField()
 
 
 async def init_db():
-    await Tortoise.init(
-        db_url=db_url, modules={"models": [__name__]}
-    )
+    await Tortoise.init(db_url=db_url, modules={"models": [__name__]})
     await Tortoise.generate_schemas()
