6f97ebcdabf9d4f1859347609d41e65dd3f595ca
iiiiii1wepfj <18629298+iiiiii1wepfj@users.noreply.github.com>
2021-09-12 10:32:59 +0300

    Revert "test"

--- bot_errors_logger.py
+++ bot_errors_logger.py
@@ -3,7 +3,6 @@ from pyrogram.types import Message
 from pyrogram.errors.exceptions.forbidden_403 import ChatWriteForbidden
 from functools import wraps
 import constants
-import requests, traceback
 
 
 def logging_errors(f):
@@ -16,13 +15,8 @@ def logging_errors(f):
             return
         except Exception as e:
             try:
-                full_trace = traceback.format_exc()
-                paste_req = requests.post("https://nekobin.com/api/documents", json={"content": full_trace})
-                pastereqjson = paste_req.json()
-                pastereqjson = pastereqjson["result"]["key"]
-                paste_url = f"https://nekobin.com/{pastereqjson}"
                 await message.reply(
-                    constants.error_msg_string.format(f"{type(e).__name__}: {e}") + f" {paste_url}",
+                    constants.error_msg_string.format(f"{type(e).__name__}: {e}"),
                     parse_mode="markdown",
                     reply_markup=constants.error_message_markup,
                 )
