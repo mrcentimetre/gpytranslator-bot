723109e231f6d18352b7ad7dfd58b4146999a392
iiiiii1wepfj <18629298+iiiiii1wepfj@users.noreply.github.com>
2021-09-19 13:54:33 +0300

    Update functions.py

--- db/functions.py
+++ db/functions.py
@@ -1,19 +1,19 @@
 from .db import users
 
 
-async def add_chat(chat_id, chat_type):
+async def add_chat(chat_id):
     await users.create(user_id=chat_id, chat_lang="en")
 
 
-async def chat_exists(chat_id, chat_type):
+async def chat_exists(chat_id):
     return await users.exists(user_id=chat_id)
 
 
-async def get_lang(chat_id: int, chat_type: str) -> str:
+async def get_lang(chat_id: int) -> str:
     return (await users.get(user_id=chat_id)).chat_lang
 
 
-async def set_lang(chat_id: int, chat_type: str, lang_code: str):
+async def set_lang(chat_id: int, lang_code: str):
     await users.filter(user_id=chat_id).update(chat_lang=lang_code)
 
 
