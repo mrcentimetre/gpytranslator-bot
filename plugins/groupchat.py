bab8d554cda85cdd136b89a252634b887c2fa87b
iiiiii1wepfj <18629298+iiiiii1wepfj@users.noreply.github.com>
2021-09-19 13:43:19 +0300

    Update groupchat.py

--- plugins/groupchat.py
+++ plugins/groupchat.py
@@ -9,6 +9,35 @@ import json
 prefix = constants.prefix
 
 
+@Client.on_message(filters.command("poll", prefix) & filters.group & filters.reply)
+@logging_errors
+async def gen_poll_tr_group_chat(bot, message: Message):
+    try:
+        if message.reply_to_message.poll:
+            replymsg = message.reply_to_message
+            poll_options = "\n".join(i["text"] for i in replymsg.poll.options)
+            txt_to_tr = f"{replymsg.poll.question}\n{poll_options}"
+            if len(message.text.split()) > 1:
+                tolanguage = message.command[1]
+            else:
+                tolanguage = "en"
+            translation = await tr(txt_to_tr, targetlang=[tolanguage, "utf-16"])
+            translation_text = translation.text
+            poll_que = translation_text.split("\n", 1)[0]
+            poll_opt = translation_text.splitlines()[1:]
+            return await bot.send_poll(
+                chat_id=message.chat.id,
+                question=poll_que,
+                options=poll_opt,
+                is_anonymous=replymsg.poll.is_anonymous,
+                allows_multiple_answers=replymsg.poll.allows_multiple_answers,
+            )
+        else:
+            return
+    except json.decoder.JSONDecodeError:
+        raise google_api_error(constants.google_tr_api_err_msg)
+
+
 @Client.on_message(
     filters.command(["tr", "tl", "translate"], prefix) & filters.group & filters.reply
 )
