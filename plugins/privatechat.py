0223532c76254738665968158d82c5302aa0b8b1
iiiiii1wepfj <18629298+iiiiii1wepfj@users.noreply.github.com>
2021-09-19 13:40:05 +0300

    Update privatechat.py

--- plugins/privatechat.py
+++ plugins/privatechat.py
@@ -52,8 +52,37 @@ async def setmylang(bot, message: Message):
         await message.reply(constants.language_text)
 
 
+@Client.on_message(filters.command("poll", prefix) & filters.private & filters.reply)
+@logging_errors
+async def gen_poll_tr_private_chat(bot, message: Message):
+    try:
+        if message.reply_to_message.poll:
+            replymsg = message.reply_to_message
+            poll_options = "\n".join(i["text"] for i in replymsg.poll.options)
+            txt_to_tr = f"{replymsg.poll.question}\n{poll_options}"
+            if len(message.text.split()) > 1:
+                tolanguage = message.command[1]
+            else:
+                tolanguage = await db.get_lang(message.chat.id, message.chat.type)
+            translation = await tr(txt_to_tr, targetlang=[userlang, "utf-16"])
+            translation_text = translation.text
+            poll_que = translation_text.split("\n", 1)[0]
+            poll_opt = translation_text.splitlines()[1:]
+            return await bot.send_poll(
+                chat_id=message.chat.id,
+                question=poll_que,
+                options=poll_opt,
+                is_anonymous=replymsg.poll.is_anonymous,
+                allows_multiple_answers=replymsg.poll.allows_multiple_answers,
+            )
+        else:
+            return
+    except json.decoder.JSONDecodeError:
+        raise google_api_error(constants.google_tr_api_err_msg)
+
+
 @Client.on_message(
-    filters.private & ~filters.command("tr", prefix) & ~filters.command("start") & ~filters.command("poll")
+    filters.private & ~filters.command("tr", prefix) & ~filters.command("start")
 )
 @logging_errors
 async def main(bot, message: Message):
